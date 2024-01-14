596d0db6a92d2aca57f1ec35434cf0cb23637e60
AiraAguado <roes.aguado.up@phinmaed.com>
2024-01-15 01:51:07 +0800

    first commit

---
+++ letter.js
@@ -0,0 +1,9 @@
+function openPopup() {
+    var popup = document.querySelector('.popup');
+    popup.classList.add('open');
+}
+
+function closePopup() {
+    var popup = document.querySelector('.popup');
+    popup.classList.remove('open');
+}
